da004738a2871daeadf3374336dd812002d184a0
= <=>
2023-06-28 19:33:50 +0800

    第一次提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -10,26 +10,112 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import os
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
 
 from torch.utils.data import DataLoader
-    
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
 
 class NeuralNetwork(nn.Module):
-    pass
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(NeuralNetwork, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = conv3x3(3, 64)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return NeuralNetwork(BasicBlock, [2, 2, 2, 2])
+
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
     # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=transform_train)
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
+                                               transform=transform_test)
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=128, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=100, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
+
 def main():
-    model = NeuralNetwork() # 若有参数则传入参数
+    # model = ResNet18().to('cuda')
+    # model = torch.nn.DataParallel(model)
+    model = NeuralNetwork(nn.Module)  # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
-    
